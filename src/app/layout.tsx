fb0995892c539186360c3baa4830fe426c6e9c19
Takeru <71886594+Takeru9016@users.noreply.github.com>
2025-02-11 08:04:10 +0000

    feat: Added Favicon and updated metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import { Be_Vietnam_Pro } from "next/font/google";
 import type { Metadata } from "next";
 
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 import "./globals.css";
 
 const beVietnamPro = Be_Vietnam_Pro({
@@ -13,6 +15,25 @@ export const metadata: Metadata = {
   title: "Eduwise Solutions - Your Path to Success",
   description:
     "Whether you're interested in IT Jobs, MBA programs or professional courses that get you job-ready, we're here to support you every step of the way!",
+  manifest: "/favicon/site.webmanifest",
+  icons: {
+    icon: [
+      { url: "/favicon/favicon.ico" },
+      { url: "/favicon/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [{ url: "/favicon/apple-touch-icon.png" }],
+    other: [
+      {
+        rel: "android-chrome-192x192",
+        url: "/favicon/android-chrome-192x192.png",
+      },
+      {
+        rel: "android-chrome-512x512",
+        url: "/favicon/android-chrome-512x512.png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -24,6 +45,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${beVietnamPro.variable} font-vietnam antialiased`}>
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
